da8357979d77e0d294fba1dd89e8751cb0bc0b78
r-kojima <kojima@sailboat.co.jp>
2022-11-27 17:57:47 +0900

    複数のメールを入力出来るように対応

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -3,7 +3,7 @@ import { Center, chakra } from '@chakra-ui/react'
 
 export const AppFooter: React.FC = (): JSX.Element => {
   return (
-    <chakra.footer mt="4" p="2">
+    <chakra.footer mt="4" p="2" bg="gray.900">
       <Center>
         <a
           href="https://github.com/RyutaKojima/mail-viewer-app"
